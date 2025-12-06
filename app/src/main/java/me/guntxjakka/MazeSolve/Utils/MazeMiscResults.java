00879b9f92a449503dcb10c2c5580bfa5baf9a28
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-12-06 15:55:24 +0700

    chore: addd ms unit for time in results

--- app/src/main/java/me/guntxjakka/MazeSolve/Utils/MazeMiscResults.java
+++ app/src/main/java/me/guntxjakka/MazeSolve/Utils/MazeMiscResults.java
@@ -5,6 +5,6 @@ import me.guntxjakka.MazeSolve.Algorithms.AlgorithmResult;
 public class MazeMiscResults {
     public static void print(AlgorithmResult res){
         System.out.println("Results: ");
-        System.out.println(String.format("Total Cost: %d, Time used: %d", res.getCost(), res.getElapsedTime()));
+        System.out.println(String.format("Total Cost: %d, Time used: %dms", res.getCost(), res.getElapsedTime()));
     }
 }
