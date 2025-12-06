bd1f9a9946ef8d369399fe53aa103c02c258e0a0
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-12-06 14:47:09 +0700

    chore: remove unused files

--- TestColor.java
+++
@@ -1,6 +0,0 @@
-public class TestColor {
-  public static void main(String[] args) {
-    System.out.println("\u001B[31mRed Text\u001B[0m");
-    System.out.println("\u001B[42mGreen Background\u001B[0m");
-  }
-}
